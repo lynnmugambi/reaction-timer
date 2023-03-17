e47303baf108473e356c3f7987a89196ec0e3263
Lynn Mugambi <mugambilynn@gmail.com>
2023-03-17 17:19:48 +0300

    Show Results

---
+++ src/components/Results.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="results">
+    <p>Reaction time: {{ time }} ms</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    time: { type: Number }
+  }
+}
+</script>
